7ebe7fec162a657fda7fd5534edede84358f003b
Stewart Williams <16945415+bravostuzero@users.noreply.github.com>
2019-08-05 13:45:29 +0100

    Update demo scripts Introduce mongo export/import make targets

--- sp142/csv_runner.py
+++ sp142/csv_runner.py
@@ -1,11 +1,29 @@
+"""
+This file runs an 'observation' by loading Configuration Data Model
+instances from a file and using them to configure a Sub-Array.
+
+This script assumes the telescope is in standby and that all
+resources are unallocated.
+"""
 import csv
 import sys
 
 import oet.domain as domain
 
+if len(sys.argv) != 2:
+    print('Usage: csv_runner <name of CSV file>')
+    sys.exit(0)
+
 scan_sequence_file = sys.argv[1]
 
+print('Starting telescope (setting DISH master devices to online)')
+telescope = domain.SKAMid()
+telescope.start_up()
+
+print('Allocating two dishes to sub-array #1')
 subarray = domain.SubArray(1)
+allocation = domain.ResourceAllocation(dishes=[domain.Dish(1), domain.Dish(2)])
+subarray.allocate(allocation)
 
 print('Reading scan sequence from {}'.format(scan_sequence_file))
 with open(scan_sequence_file, 'r') as csv_file:
@@ -17,4 +35,5 @@ with open(scan_sequence_file, 'r') as csv_file:
         subarray.configure_from_file(exported_cdm)
 
         print('Scanning for {} seconds'.format(scan_duration))
+        print('### NO-OP AS SubArrayNode.Scan() IS NOT IMPLEMENTED YET ###')
         # subarray.scan(scan_duration)
--- sp142/interactive_commands.py
+++ sp142/interactive_commands.py
@@ -1,8 +1,35 @@
+"""
+This file contains an example itango session that exercises the OET and TMC
+functionality for SP-142.
+
+To run these commands, start the integrated environment and connect to the
+OET with:
+
+  make ds-config
+  make import_dashboards
+  make mvp
+  make oet
+  docker attach oet
+
+... and the commands at the command prompt.
+
+"""
+
+# start up the telescope, turning DISH master devices on
 telescope = SKAMid()
 telescope.start_up()
 
+# Create a sub-array and allocate two dishes to it
 subarray = SubArray(1)
-subarray.allocate(ResourceAllocation(dishes=[Dish(1), Dish(2)]))
+allocation = ResourceAllocation(dishes=[Dish(1), Dish(2)])
+subarray.allocate(allocation)
 
-polaris = SubArrayConfiguration(SkyCoord.from_name('polaris'), 'polaris', receiver_band=1)
+# Configure the sub-array to point at Polaris using RX band 1
+polaris = SubArrayConfiguration(SkyCoord.from_name('polaris'), 'polaris', receiver_band='1')
 subarray.configure(polaris)
+
+# (optional) deallocate all sub-array resources
+subarray.deallocate()
+
+# (optional) send the telescope to STANDBY
+telescope.standby()
